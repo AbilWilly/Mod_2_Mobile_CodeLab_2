cd2beb6edc82cd5e65e78b5e9fc076ce09d12ddd
AbilWilly <abilwildan2003@gmail.com>
2024-10-11 16:37:22 +0700

    coba commit

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:modul_1_mobile/app/routes/app_pages.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({super.key});
+  const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
+        title: Text("HomePage"),
+        actions: [
+          PopupMenuButton(
+              onSelected: (value) {
+                switch (value) {
+                  case 0:
+                    Get.toNamed(Routes.HTTP);
+                    break;
+                }
+              },
+              itemBuilder: (context) => [
+                const PopupMenuItem(
+                  value: 0,
+                  child: TextButton(
+                    onPressed: null,
+                    child: Text("HTTP Page"),
+                  ),
+                ),
+              ])
+
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("HOW TO USE THIS APPLICATION"),
+          Text("1. Buka icon titik 3 di pojok kanan atas"),
+          Text("2. Pilih Package yang ingin kalian liat sebagai outputnya"),
+        ],
       ),
     );
   }
